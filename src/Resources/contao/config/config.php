e44bdd528b07db78a7cbac470e66e795810226c8
Konstantin Wagner <k.wagner@heimrich-hannot.de>
2018-06-27 11:39:20 +0200

    updated config and composer.json

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 if (\Contao\System::getContainer()->get('huh.utils.container')->isFrontend()) {
-    $GLOBALS['TL_JAVASCRIPT']['huh_components_glightbox_gallery'] = 'assets/glightbox/js/glightbox.min.js|static';
-    $GLOBALS['TL_JAVASCRIPT']['lightbox-gallery']                 = 'bundles/heimrichhannotlightboxgallery/js/glightbox.min.js|static';
-    $GLOBALS['TL_USER_CSS']['huh_components_glightbox_gallery']   = 'assets/glightbox/css/glightbox.css|static';
+    $GLOBALS['TL_JAVASCRIPT']['huh_components_glightbox_gallery'] = 'assets/glightbox-gallery/js/glightbox.min.js|static';
+    $GLOBALS['TL_JAVASCRIPT']['lightbox-gallery']                 = 'bundles/heimrichhannotlightboxgallery/js/contao-lightbox-gallery-bundle.min.js|static';
+    $GLOBALS['TL_USER_CSS']['huh_components_glightbox_gallery']   = 'assets/glightbox-gallery/css/glightbox.min.css|static';
 }
