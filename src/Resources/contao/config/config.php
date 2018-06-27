f371e26c77deb48043197c07b73dc4071c8bb392
Konstantin Wagner <k.wagner@heimrich-hannot.de>
2018-06-27 11:23:01 +0200

    updated composer

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,8 +1,7 @@
 <?php
 
 if (\Contao\System::getContainer()->get('huh.utils.container')->isFrontend()) {
-    $GLOBALS['TL_JAVASCRIPT']['mcstudios-glightbox'] = 'dist/js/glightbox.min.js|static';
-    $GLOBALS['TL_JAVASCRIPT']['lightbox-gallery']    = 'bundles/heimrichhannotlightboxgallery/js/glightbox.min.js|static';
-    // css
-    $GLOBALS['TL_USER_CSS']['lightbox-gallery'] = 'dist/css/glightbox.css|static';
+    $GLOBALS['TL_JAVASCRIPT']['huh_components_glightbox_gallery'] = 'assets/glightbox/js/glightbox.min.js|static';
+    $GLOBALS['TL_JAVASCRIPT']['lightbox-gallery']                 = 'bundles/heimrichhannotlightboxgallery/js/glightbox.min.js|static';
+    $GLOBALS['TL_USER_CSS']['huh_components_glightbox_gallery']   = 'assets/glightbox/css/glightbox.css|static';
 }
